6a4c119b8365a4ce0ec32406a22bb92eb6cd4478
NikitaTutik <famounst@gmail.com>
2022-02-06 20:03:36 +0300

    env variables, api connection

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,7 @@ RUN mkdir /movies
 WORKDIR /movies/
 COPY requirements.txt /movies/
 RUN pip install -r requirements.txt 
+ARG IMDB_KEY
+ENV IMDB_KEY=${IMDB_KEY}
 CMD ["python3", "manage.py", "runserver", "0.0.0.0:8000"]
 COPY . /movies/
--- main/views.py
+++ main/views.py
@@ -1,3 +1,28 @@
+import os
 from django.shortcuts import render
+from .forms import MovieSearch
+import requests
 
-# Create your views here.
+def index(request):
+    if request.method == 'GET':
+        form = MovieSearch(request.GET)
+
+        url = "https://imdb8.p.rapidapi.com/auto-complete"
+
+        querystring = {"q":"game of thr"}
+
+        headers = {
+            'x-rapidapi-host': "imdb8.p.rapidapi.com",
+            'x-rapidapi-key': os.environ.get('IMDB_KEY')
+            }
+
+        response = requests.request("GET", url, headers=headers, params=querystring)
+
+        APIdata = response.json()
+
+        print(APIdata) #temp test
+        context = {'form': form}
+        return render(request, 'main/index.html', context)
+    else:
+        return render(request, 'main/index.html')
+
