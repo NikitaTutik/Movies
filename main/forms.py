39140479e1ec30877ca0654d5fae9cb6a4d9f75c
NikitaTutik <famounst@gmail.com>
2022-02-13 19:11:29 +0300

    fixes

--- main/forms.py
+++ main/forms.py
@@ -1,4 +1,4 @@
 from django import forms
 
 class MovieSearch(forms.Form):
-    movie_name = forms.CharField(max_length=50,label='')
+    movie_name = forms.CharField(max_length=50,label='', widget=forms.TextInput(attrs={'class': 'form-control'}))
